aa802cf736325fbb4852e506bd0da7ed84c08d46
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-04-06 10:57:40 +1000

    question answered

--- ex4.rb
+++ ex4.rb
@@ -1,2 +1,9 @@
 # Write a method that takes one argument, a number, and that returns true if the number is divisible by 5, and false otherwise.
 
+def divisible_by_five(num)
+    num % 5 == 0 ? true : false
+end
+
+p divisible_by_five(10)
+
+p divisible_by_five(6)
